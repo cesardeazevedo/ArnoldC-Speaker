82d213cc46116dbea7ed241ce0d3b61843d46304
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-10-01 23:06:11 -0300

    working in progress

--- app/src/controller.js
+++ app/src/controller.js
@@ -1,11 +1,20 @@
-
-angular.module('myApp.controllers', []);
-angular.module('myApp.controllers').
+angular.module('myApp.controllers', []).
     controller('SpeakerController', SpeakerController);
 
     SpeakerController.$inject = ['$scope'];
     function SpeakerController($scope)
     {
+        $scope.code = 'IT\'S SHOWTIME\n'+
+        'TALK TO THE HAND "hello world"\n'+
+        'YOU HAVE BEEN TERMINATED';
+
         var editor = ace.edit("editor");
+        editor.setValue($scope.code);
+        editor.clearSelection();
+        editor.on('change', function(e){
+            $scope.$apply(function(){
+                $scope.code = editor.getValue();
+            });
+        });
     }
 
