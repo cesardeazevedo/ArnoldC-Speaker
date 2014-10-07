fef7d016ed5729f95edb0e0c0f5aa13a35b3b508
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-10-06 21:06:08 -0300

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 
-var gulp    = require('gulp'),
-    concat  = require('gulp-concat'),
-    uglify  = require('gulp-uglify'),
-    jshint  = require('gulp-jshint'),
-    filter  = require('gulp-filter'),
-    jade    = require('gulp-jade'),
-    bower   = require('main-bower-files'),
-    stylus  = require('gulp-stylus'),
-    connect = require('gulp-connect'),
-    nib     = require('nib');
+var gulp     = require('gulp'),
+    concat   = require('gulp-concat'),
+    uglify   = require('gulp-uglify'),
+    jshint   = require('gulp-jshint'),
+    filter   = require('gulp-filter'),
+    jade     = require('gulp-jade'),
+    imgmin   = require('gulp-imagemin'),
+    bower    = require('main-bower-files'),
+    stylus   = require('gulp-stylus'),
+    connect  = require('gulp-connect'),
+    nib      = require('nib'),
+    fallback = require('connect-history-api-fallback');
 
 var paths = {
-    destjs: 'app/dist/src',
-    destcss: 'app/dist/css',
-    dest: 'app/dist',
-    js:   'app/src/**/*.js',
-    styl: 'app/styles/*.styl',
-    jade: 'app/*.jade'
+    destjs:     'app/dist/src',
+    destcss:    'app/dist/css',
+    destaudio:  'app/dist/audio',
+    destimages: 'app/dist/images',
+    dest:   'app/dist',
+    js:     'app/src/**/*.js',
+    styl:   'app/styles/*.styl',
+    audio:  'app/audio/*.mp3',
+    images: 'app/images/*.png',
+    jade:  'app/*.jade'
 }
 
 gulp.task('scripts', function(){
@@ -44,6 +50,20 @@ gulp.task('views', function(){
     .pipe(jade())
     .pipe(gulp.dest(paths.dest))
     .pipe(connect.reload());
+});
+
+gulp.task('images', function(){
+    return gulp.src(paths.images)
+    .pipe(imgmin({
+            optimizationLeval: 3,
+            progressive: true
+    }))
+    .pipe(gulp.dest(paths.destimages));
+});
+
+gulp.task('audios', function(){
+    return gulp.src(paths.audio)
+    .pipe(gulp.dest(paths.destaudio));
 });
 
 gulp.task('bower', function(){
@@ -74,14 +94,16 @@ gulp.task('server', ['default', 'watch'],function(){
         port: 9090,
         root: 'app/dist',
         livereload: true,
-        // middleware: function(connect, opt){
-            // return [history];
-        // }
+        middleware: function(){
+            return [fallback];
+        }
     });
 });
 
 gulp.task('watch', function() {
-    gulp.watch([paths.styl, paths.js, paths.jade], ['styles', 'views', 'scripts' ]);
+    gulp.watch([paths.styl, paths.js, paths.jade, paths.images], ['styles', 'views', 'scripts' ]);
 });
 
-gulp.task('default', ['scripts', 'styles', 'views', 'bower']);
+gulp.task('default', ['scripts', 'styles', 'views', 'images']);
+
+gulp.task('build', ['bower','audios','default']);
